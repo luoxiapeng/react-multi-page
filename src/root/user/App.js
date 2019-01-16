2334ab45d1af7492cdf7741c537e2106b03558a6
郑活朝 <442597584@qq.com>
2019-01-16 15:40:01 +0800

    修改

--- src/root/user/App.js
+++ src/root/user/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { List, InputItem, WhiteSpace } from 'antd-mobile';
+import { InputItem} from 'antd-mobile';
 import style from './App.module.scss';
 console.log(style)
 class App extends Component {
